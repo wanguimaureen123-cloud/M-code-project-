77c918cc7c2135ae9001debf61f57819302856a2
wanguimaureen123-cloud <wanguimaureen123@gmail.com>
2025-09-18 13:21:27 +0300

    Add files via upload

---
+++ Maureen CT 100 program .c
@@ -0,0 +1,26 @@
+// Simple c program (units for this semester)
+/*
+Name:MwaMaureen Wangui 
+Reg no:CT100/G/26135/25
+Unit SPC 2101
+course:BSCIT
+Description:Units for this semester 
+*/
+
+#include<stdio.h>
+//main function 
+
+int main (){
+    printf("my units for this semester\n");
+    printf("1.SPH 2107 Analogue Electronics\n");
+    printf("2.SPS 2102 Computer systems and organisation\n");
+    printf("3.SPM 2104 Mathematics for science\n");
+    printf("4.SPC 2101 Introduction to programming and Alogarithm\n");
+    printf("5.UCU 2106 Communication skills\n");
+    printf("6.UCU 2105 Fundamental of ICT\n");
+    printf("7.SPI 2120 Essential for Accounting\n");
+    printf("8.SPI 2103 Introduction to computer application\n");
+    return 0;
+    }
+    
+    
