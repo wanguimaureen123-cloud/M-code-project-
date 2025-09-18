9d71bd8a8b0d2155c9697dc4b926ea4399d84d2f
wanguimaureen123-cloud <wanguimaureen123@gmail.com>
2025-09-18 14:36:39 +0300

    Add files via upload

---
+++ Project 8099.c
@@ -0,0 +1,27 @@
+// Simple c program 
+/*
+Name:Mwaniki Maureen Wangui 
+Reg no:CT100/G/26135/25
+Description:Give Assingment
+*/
+#include<stdio.h>
+
+//main function 
+int main() {
+    int height;
+    printf("enter your height:\n");
+    scanf("%d",& height); 
+    printf("your height is%d\n",6);
+    
+    int phonenumber;
+    printf("enter your phone number\n");
+    scanf("%d",&phonenumber);
+    printf("your phone number is %d\n",700254019);
+    
+    int weight;
+    printf("enter your weight");
+    scanf("%d",& weight);
+    printf("your weight is%d \n",53);
+    
+    return 0;
+    }
