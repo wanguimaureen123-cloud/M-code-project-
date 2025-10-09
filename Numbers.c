557807785cd35a63d25a9217392dd86c258e7a78
wanguimaureen123-cloud <wanguimaureen123@gmail.com>
2025-10-09 11:26:28 +0300

    Add files via upload

---
+++ Numbers.c
@@ -0,0 +1,14 @@
+/*
+Name:Mwaniki Maureen Wangui 
+Reg no:CT100/G/26135/25
+unit Spc2101 programming 
+Description:Numbers 50 to 100
+*/
+
+#include <stdio.h>
+int main(){
+   for(int i =100;i >=50;i--){
+   printf("%d/n",i);
+   }
+   return 0;
+   }
